437b373f225159164b2ec7dd01342152b6b5ee60
Aravinda Rathnayake <aravindacloud@gmail.com>
2020-02-04 18:18:34 +0530

    Initial Commit

---
+++ wiki-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wiki-app
+//
+//  Created by Aravinda Rathnayake on 2/4/20.
+//  Copyright © 2020 Aravinda Rathnayake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ wiki-appTests/wiki_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  wiki_appTests.swift
+//  wiki-appTests
+//
+//  Created by Aravinda Rathnayake on 2/4/20.
+//  Copyright © 2020 Aravinda Rathnayake. All rights reserved.
+//
+
+import XCTest
+@testable import wiki_app
+
+class wiki_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
