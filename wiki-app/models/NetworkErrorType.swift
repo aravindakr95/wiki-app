130523427f7d9ebec94db060680b5479771ffa4f
Aravinda Rathnayake <aravindacloud@gmail.com>
2020-02-05 09:22:21 +0530

    Separate ApiRequestFetcher and NetworkError

---
+++ wiki-app/models/NetworkErrorType.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkErrorType.swift
+//  wiki-app
+//
+//  Created by Aravinda Rathnayake on 2/5/20.
+//  Copyright © 2020 Aravinda Rathnayake. All rights reserved.
+//
+
+import Foundation
+
+public enum NetworkErrorType: Error {
+    case failure
+    case success
+}
--- wiki-app/services/APIRequestFetcher.swift
+++ wiki-app/services/APIRequestFetcher.swift
@@ -10,15 +10,10 @@ import Foundation
 import SwiftyJSON
 import Alamofire
 
-enum NetworkError: Error {
-    case failure
-    case success
-}
-
 class APIRequestFetcher {
     var searchResults = [JSON]()
     
-    func search(searchText: String, completionHandler: @escaping ([JSON]?, NetworkError) -> ()) {
+    func search(searchText: String, completionHandler: @escaping ([JSON]?, NetworkErrorType) -> ()) {
         let urlToSearch = "https://en.wikipedia.org//w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=150&pilimit=10&wbptterms=description&gpssearch=\(searchText)&gpslimit=10"
         
         Alamofire.request(urlToSearch).responseJSON { response in
@@ -38,7 +33,7 @@ class APIRequestFetcher {
         }
     }
     
-    func fetchImage(url: String, completionHandler: @escaping (UIImage?, NetworkError) -> ()) {
+    func fetchImage(url: String, completionHandler: @escaping (UIImage?, NetworkErrorType) -> ()) {
         Alamofire.request(url).responseData { responseData in
             
             guard let imageData = responseData.data else {
